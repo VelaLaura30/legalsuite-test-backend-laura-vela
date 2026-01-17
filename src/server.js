a009e941e4cebf482e7c16948369debb3d472796
VelaLaura30 <laura.vela03@uptc.edu.co>
2026-01-17 16:54:09 -0500

    Configuración de Sequelize y la conexión a la base de datos

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import app from './app.js';
+import { connectDatabase } from './config/database.js';
+
 
 const PORT = process.env.PORT || 3000;
+
+
+const startServer = async () => {
+    await connectDatabase();
+}
+
 
 app.listen(PORT, () => {
   console.log(` Servidor ejecutandose en el puerto ${PORT}`);
 });
+
+
+startServer();
